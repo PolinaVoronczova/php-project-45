c32ca00828b6831d88b5eb7b14113bbf2276a6ca
VoronczovaPolina <tirabyrius@gmail.com>
2023-03-13 21:21:28 +0500

    add PHP_CodeSniffer

--- src/Cli.php
+++ src/Cli.php
@@ -1,14 +1,13 @@
 <?php
-//Объявляю пространство имен BrainGames\Cli
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
+
 function hello()
 {
-        
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
-
