684dfd15c08e8999170652e24ca6e4a2fb3aea17
PolinaVoronczova <125808091+PolinaVoronczova@users.noreply.github.com>
2023-04-04 20:08:31 +0500

    Update even.php

--- src/Games/even.php
+++ src/Games/even.php
@@ -5,6 +5,6 @@ function evenAskAndGetCorrectAnswer()
 {
     $randNum = rand(1, 100);
     $correctAnswer =  $randNum % 2 === 0 ? 'yes' : 'no';
-    line('Question:'.$randNum);
+    line('Question: '.$randNum);
     return $correctAnswer;
 }
